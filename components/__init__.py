be8fc04717dd66391d4a534798757365c1d8cb83
1abhi6 <1abhigup6@gmail.com>
2023-06-05 14:21:59 +0530

    Added component package

---
+++ components/__init__.py
@@ -0,0 +1,1 @@
+from components.group import GroupPlot
---
+++ components/group.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 12:38:27 2023
+
+@author: Abhishek Gupta
+"""
+
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+
+from analysis import GroupSpecificAnalysis
+
+
+class PlotBarChart:
+    def __init__(self, df, x_axis, y_axis):
+        fig = px.bar(df, x='name', y='amount')
+        st.plotly_chart(fig, use_container_width=True)
+
+
+class GroupPlot:
+    def __init__(self, df, selected_user):
+        self.df = df
+        self.selected_user = selected_user
+        self.group_specific_analysis = GroupSpecificAnalysis(
+            self.df, self.selected_user)
+
+    def plot_most_active_users(self):
+        users = self.group_specific_analysis.most_active_users()
+        PlotBarChart(users, 'User Name', 'Number of Chats')
